e50cc638584c0a3929e555f240a3a432c74a78c7
Wang <645565009@qq.com>
2025-04-18 11:39:48 +0800

    feat: contractor quick command

--- src/main/java/com/ginkgooai/core/mcp/contractor/dto/request/QueryContractorRequest.java
+++ src/main/java/com/ginkgooai/core/mcp/contractor/dto/request/QueryContractorRequest.java
@@ -15,12 +15,8 @@ import java.util.List;
 @NoArgsConstructor
 public class QueryContractorRequest{
 
-    @Schema(description = "The address of the contractor", example = "123 Main St", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @Schema(description = "The address of the contractor, street + city + state", example = "P O BOX 1027,SAN CARLOS,CA", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String address;
-    @Schema(description = "The city of the contractor", example = "New York", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    private String city;
-    @Schema(description = "The state of the contractor", example = "NY", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    private String state;
     @Schema(description = "The classifications of the contractor", example = "C10", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private List<String> classifications;
     @Schema(description = "The radius of the contractor, meter as unit", example = "100", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
