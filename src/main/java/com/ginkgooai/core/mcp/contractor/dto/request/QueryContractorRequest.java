58a1ee369c5c4fa5af71a899fcfff42524dc11bc
Wang <645565009@qq.com>
2025-04-22 22:12:56 +0800

    feat: contractor quick command use card

--- src/main/java/com/ginkgooai/core/mcp/contractor/dto/request/QueryContractorRequest.java
+++ src/main/java/com/ginkgooai/core/mcp/contractor/dto/request/QueryContractorRequest.java
@@ -19,7 +19,7 @@ public class QueryContractorRequest{
     private String address;
     @Schema(description = "The classifications of the contractor", example = "C10", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private List<String> classifications;
-    @Schema(description = "The radius of the contractor, meter as unit, default value 80467.2 meters", defaultValue = "80467.2", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @Schema(description = "The radius of the contractor, meter as unit", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private Double radius;
 
 }
